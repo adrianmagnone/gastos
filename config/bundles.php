ce43878bc924cd4566506398508dca44a77f16ef
Adrian Magnone <adrianvmagnone@gmail.com>
2023-10-23 16:08:39 -0300

    Laravel 9

---
+++ config/bundles.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+    'js' => [
+        'jQuery' => [
+            'js/jquery.min.js'
+        ],
+        'tabler' => [
+            'js/tabler.min.js',
+            'js/main.min.js',
+        ],
+        'dataTable' => [
+            'js/datatables.min.js'
+        ],
+        'calendar' => [
+            'js/calendar.min.js'
+        ],
+        'search' => [
+            'js/search.min.js'
+        ],
+        'wraps' => [
+            'js/wraps.min.js'
+        ],
+        'code' => [
+            'js/code.min.js'
+        ]
+    ],
+
+    'css' => [
+        'tabler' => [
+            'css/tabler.min.css',
+            'https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css'
+        ],
+        'dataTable' => [
+            'css/datatables.min.css'
+        ],
+        'calendar' => [
+            'css/calendar.min.css'
+        ],
+    ]
+];
