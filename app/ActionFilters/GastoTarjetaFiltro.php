e1bcf836d089d96d9f14b2b5ae72091851198e76
Adrian Magnone <adrianvmagnone@gmail.com>
2024-01-04 22:54:39 -0300

    Se agrega el filtro por periodo

--- app/ActionFilters/GastoTarjetaFiltro.php
+++ app/ActionFilters/GastoTarjetaFiltro.php
@@ -2,17 +2,23 @@
  
 namespace App\ActionFilters;
 
+use App\Helpers\DateHelper as MiDate;
 class GastoTarjetaFiltro extends \App\Lib\Actions\FilterBase
 {
     protected function setFiltersKeys()
     {
-        return [ 'tarjeta', 'estado' ];
+        return [ 'tarjeta', 'estado', 'periodo' ];
     }
 
 
     protected function filtroTarjeta(&$query, $value)
     {
         $query->where('tarjeta_id', (int)$value);
+    }
+
+    protected function filtroPeriodo(&$query, $value)
+    {
+        $query->where('periodoInicial', '<=',  MiDate::fromFormatTo('d/m/Y', $value, 'Y-m-d'));
     }
 
     protected function filtroEstado(&$query, $value)
