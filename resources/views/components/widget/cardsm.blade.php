0874a44167c097aeb585c30501f92f4b83fcec08
Adrian Magnone <adrianvmagnone@gmail.com>
2024-05-01 10:20:00 -0300

    Componente para tarjetas chicas

---
+++ resources/views/components/widget/cardsm.blade.php
@@ -0,0 +1,23 @@
+<div class="col-md-{{ $md }} col-xl-{{ $xl }}">
+    <div class="card card-sm">
+        <div class="card-body">
+            <a href="{{ route($route) }}">
+                <div class="row align-items-center">
+                    <div class="col-auto">
+                        <span class="bg-primary text-white avatar">
+                            <i class="icon ti {{ $icon }}"></i>
+                        </span>
+                    </div>
+                    <div class="col">
+                        <div class="font-weight-medium">
+                            {{ $titulo }}
+                        </div>
+                        <div class="text-muted">
+                            {{ $subtitulo }}
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+  </div>
