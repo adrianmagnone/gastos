1d992dd37a3fc726e19155accec6cf747ac764e3
Adrian Magnone <adrianvmagnone@gmail.com>
2026-04-03 11:53:24 -0300

    Se actualiza codigo

--- resources/js/wraps/wrap-select.js
+++ resources/js/wraps/wrap-select.js
@@ -2,58 +2,138 @@ class wrapSelect
 {
 	constructor(selector, functionChaged)
 	{
-        this.select = new TomSelect(selector, {
-            create: false,
-            allowEmptyOption: true,
-            closeAfterSelect: true,
-            valueField: 'id',
-            searchField: 'text'
-        });
+        this.selector = $(selector);
 
-        this.select.on('change', function(){
+        if (this.selector.count == 0)
+        {
+            this.innerControl = false;
+            return;
+        }
 
-			if (typeof functionChaged === 'function')
-	    	{
-    			functionChaged();
-    		}
-		});
+        if (this.selector.is("select"))
+        {    
+            this.select = new TomSelect(selector, {
+                // Determines if the user is allowed to create new items
+                create: false,
+                allowEmptyOption: true,
+                // Force the dropdown to close after selections are made
+                closeAfterSelect: true,
+                // The tab key will choose the currently selected item
+                selectOnTab: true,
+                valueField: 'id',
+                labelField: 'text',
+                searchField: 'text'
+            });
+
+            //this.select = this.selector.selectize();
+
+            if (typeof functionChaged === 'function')
+            {
+                this.selector.on('change', () => functionChaged());
+            }
+        }
+
+        this.innerControl = document.getElementById(selector.substring(1));
     }
 
     getValue()
     {
-    	return this.select.getValue();
+        if (this.innerControl)
+            return this.innerControl.value;
+
+        return '';
     }
 
     getText()
     {
-        const value = this.getValue();
+        if (this.innerControl)
+        {
+            let tag = this.innerControl.tagName;
 
-        return this.select.getOption(value).textContent;
+            if (tag == 'SELECT')
+            {
+                return this.innerControl.options[this.innerControl.selectedIndex].text
+            }
+
+            if (tag == 'INPUT')
+            {
+                return this.innerControl.value;
+            }
+        }
+        return '';
     }
 
     set(value)
     {
-        this.select.setValue(value, true);
+        if (this.innerControl)
+        {
+            let tag = this.innerControl.tagName;
+
+            if (tag == 'SELECT')
+            {
+                this.innerControl.value = value;
+                this.select.setValue(value);
+            }
+
+            if (tag == 'INPUT')
+            {}
+        }
+    }
+
+    hasOptions()
+    {
+        if (this.innerControl)
+            return this.innerControl.options;
+
+        return false;
+    }
+
+    data(name)
+    {
+        if (this.innerControl)
+        {
+            let currentOption = this.innerControl.options[this.innerControl.selectedIndex];
+
+            return currentOption.dataset[name];
+        }
+
+        return '';
     }
 
     changeItems(items)
     {
-        this.select.clear(true);
-
-        this.select.clearOptions();
-
-        if (items)
+        if (this.innerControl)
         {
-            for (let item of items)
+            let tag = this.innerControl.tagName,
+                value = this.innerControl.value;
+
+
+            if (tag == 'SELECT')
             {
-                this.select.addOption({
-                    id: item.id,
-                    title: item.descripcion
-                },false);
+                this.innerControl.innerHTML = '';
+
+                if (items)
+                {
+                    for (let item of items)
+                    {
+                        this.innerControl.options.add(new Option(item.descripcion, item.id));
+                    }
+                }
+
+                this.innerControl.value = value;
+            }
+
+            if (tag == 'INPUT')
+            {
+                // PONER EL VALOR EN EL INPUT
             }
         }
-
-        this.select.refreshOptions(false);
+    }
+    
+    focus()
+    {
+        if (this.select)
+            this.select.focus();
     }
 };
 
